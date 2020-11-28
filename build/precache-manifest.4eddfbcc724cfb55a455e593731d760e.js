d4f5b789f222ddfddea63db49901a7cbe1ceafc1
violetbaggins <violetbaggins@gmail.com>
2020-11-28 11:34:23 -0300

    commit para deploy 2

---
+++ build/precache-manifest.4eddfbcc724cfb55a455e593731d760e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4db42001f5b0f2fcb39262f9e66549",
+    "url": "/react_con_APIs/index.html"
+  },
+  {
+    "revision": "2bcda25b38c61d29e281",
+    "url": "/react_con_APIs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b10a833c3f6e285d7b4a",
+    "url": "/react_con_APIs/static/js/2.4f485f3e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_con_APIs/static/js/2.4f485f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcda25b38c61d29e281",
+    "url": "/react_con_APIs/static/js/main.93e8f132.chunk.js"
+  },
+  {
+    "revision": "e3921aa422cedbf68b08",
+    "url": "/react_con_APIs/static/js/runtime-main.8194ad5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_con_APIs/static/media/logo.5d5d9eef.svg"
+  }
+]);
